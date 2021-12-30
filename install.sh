04c15dc19f75fc144783db41405df75b6dfe4991
redroot <luke.williams@carwow.co.uk>
2021-12-30 17:40:24 +0000

    the first commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# aliases
+
+alias cw=carwow
+alias cst='carwow start'
+alias csp='carwow stop'
+alias crs='carwow restart'
+alias cru='carwow run'
+alias cbu='carwow run bundle'
+alias cry='carwow run yarn'
+alias cwp='carwow webpack'
+
+
+# env variables
+export GITHUB_PACKAGES_TOKEN=$PACKAGES_TOKEN
+
+# git env 
+source ./prompt
